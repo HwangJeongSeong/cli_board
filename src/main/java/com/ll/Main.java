e7628f104988c0c8c8693b59a9ce7570c008928a
jeongseong <hjs54336297@gmail.com>
2025-07-11 18:40:05 +0900

    4단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,14 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in); // Scanner는 루프 밖에서 한 번만 생성
+        System.out.println(" ______              _      ______                          _ ");
+        System.out.println("|  _  \\            | |     | ___ \\                        | |");
+        System.out.println("| | | |  __ _  ___ | |__   | |_/ /  ___    __ _  _ __   __| |");
+        System.out.println("| | | | / _` |/ __|| '_ \\  | ___ \\ / _ \\  / _` || '__| / _` |");
+        System.out.println("| |/ / | (_| |\\__ \\| | | | | |_/ /| (_) || (_| || |   | (_| |");
+        System.out.println("|___/   \\__,_||___/|_| |_| \\____/  \\___/  \\__,_||_|    \\__,_|");
+        System.out.println("                                                             ");
+        System.out.println("                                                             ");
+
+        System.out.println("Select");
+        Scanner scn = new Scanner(System.in);
+        Plus add = new Plus();
+
         while (true) {
-            String input = scn.nextLine();
-            if (input.equals("종료")) {
-                System.out.println("종료");
+            System.out.println("명령) 등록 (종료하려면 '종료' 입력)");
+            String title = scn.nextLine();
+            if (title.equals("종료")) {
+                System.out.println("종료합니다.");
                 break;
             }
+
+            System.out.print("내용 입력: ");
+            String script = scn.nextLine();
+
+            System.out.println("제목: " + title);
+            System.out.println("내용: " + script);
+            System.out.println("게시판 번호: " + add.num());
         }
-        scn.close();
+    }
+}
+
+class Plus {
+    int a = 1;
+
+    int num() {
+        return a++;
     }
 }
