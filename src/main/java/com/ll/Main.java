efc6ca16846e8cc9f010ad877a61df890d27ecb4
jeongseong <hjs54336297@gmail.com>
2025-07-14 15:14:18 +0900

    5단게

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,44 +1,24 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" ______              _      ______                          _ ");
-        System.out.println("|  _  \\            | |     | ___ \\                        | |");
-        System.out.println("| | | |  __ _  ___ | |__   | |_/ /  ___    __ _  _ __   __| |");
-        System.out.println("| | | | / _` |/ __|| '_ \\  | ___ \\ / _ \\  / _` || '__| / _` |");
-        System.out.println("| |/ / | (_| |\\__ \\| | | | | |_/ /| (_) || (_| || |   | (_| |");
-        System.out.println("|___/   \\__,_||___/|_| |_| \\____/  \\___/  \\__,_||_|    \\__,_|");
-        System.out.println("                                                             ");
-        System.out.println("                                                             ");
-
-        System.out.println("Select");
+        System.out.println("== 게시판 앱 ==");
         Scanner scn = new Scanner(System.in);
-        Plus add = new Plus();
+        ArticleManager manager = new ArticleManager(scn);
 
         while (true) {
-            System.out.println("명령) 등록 (종료하려면 '종료' 입력)");
-            String title = scn.nextLine();
-            if (title.equals("종료")) {
-                System.out.println("종료합니다.");
-                break;
-            }
+            System.out.print("명령) ");
+            String cmd = scn.nextLine().trim();
 
-            System.out.print("내용 입력: ");
-            String script = scn.nextLine();
-
-            System.out.println("제목: " + title);
-            System.out.println("내용: " + script);
-            System.out.println("게시판 번호: " + add.num());
+            if (cmd.equals("종료")) break;
+            else if (cmd.equals("등록")) manager.register();
+            else if (cmd.equals("목록")) manager.printList();
+            else if (cmd.startsWith("삭제?id=")) manager.delete(cmd);
+            else if (cmd.startsWith("수정?id=")) manager.update(cmd);
+            else System.out.println("올바르지 않은 명령입니다.");
         }
-    }
-}
-
-class Plus {
-    int a = 1;
-
-    int num() {
-        return a++;
     }
 }
---
+++ src/main/java/com/ll/RunTheDashBoard.java
@@ -0,0 +1,120 @@
+package com.ll;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Article {
+    protected int id;
+    protected String subject;
+    protected String content;
+
+    public Article(int id, String subject, String content) {
+        this.id = id;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+class ArticleManager extends getContent {
+    private ArrayList<Article> articles = new ArrayList<>();
+    private int nextId = 1;
+    private Scanner scn;
+
+    public ArticleManager(Scanner scn) {
+        this.scn = scn;
+    }
+
+    public void register() {
+        System.out.print("제목 : ");
+        String subject = scn.nextLine();
+        System.out.print("내용 : ");
+        String content = scn.nextLine();
+
+        Article article = new Article(nextId, subject, content);
+        articles.add(article);
+        System.out.println(nextId + "번 게시글이 등록되었습니다.");
+        nextId++;
+    }
+
+    public void printList() {
+        System.out.println("번호 / 제목 / 내용");
+        System.out.println("----------------------");
+        for (int i = articles.size() - 1; i >= 0; i--) {
+            Article a = articles.get(i);
+            System.out.println(a.getId() + " / " + a.getSubject() + " / " + a.getContent());
+        }
+    }
+
+    public void delete(String cmd) {
+        int id = parseId(cmd);
+        Article target = findById(id);
+        if (target != null) {
+            articles.remove(target);
+            System.out.println(id + "번 게시물이 삭제되었습니다.");
+        } else {
+            System.out.println(id + "번 게시물이 존재하지 않습니다.");
+        }
+    }
+
+    public void update(String cmd) {
+        int id = parseId(cmd);
+        Article target = findById(id);
+        if (target != null) {
+            System.out.println("제목(기존) : " + target.getSubject());
+            System.out.print("제목 : ");
+            target.setSubject(scn.nextLine());
+
+            System.out.println("내용(기존) : " + target.getContent());
+            System.out.print("내용 : ");
+            target.setContent(scn.nextLine());
+        } else {
+            System.out.println(id + "번 게시물이 존재하지 않습니다.");
+        }
+    }
+
+    private int parseId(String cmd) {
+        return Integer.parseInt(cmd.split("=")[1]);
+    }
+
+    private Article findById(int id) {
+        for (Article a : articles) {
+            if (a.getId() == id) return a;
+        }
+        return null;
+    }
+}
+class getId extends getSubject {
+    public getId() {
+        super();
+    }
+}
+
+class getSubject extends getContent {
+    public getSubject() {
+        super();
+    }
+}
+
+class getContent {
+    public getContent() {
+    }
+}
